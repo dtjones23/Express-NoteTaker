aa060ba5af023f32dbfbe0e57c90fc3297ce2583
DeAngelo Jones <deangelojones0708@gmail.com>
2023-11-28 19:20:22 -0600

    added html route to server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const htmlRoute = require('./routes/html')
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // serves the static files from the public folder
 app.use(express.static('public'));
+app.use(htmlRoute)
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
