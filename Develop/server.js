679f3a53c5f7e0e3bf38a45add20bc0893d40322
DeAngelo Jones <deangelojones0708@gmail.com>
2023-11-28 19:15:38 -0600

    fixed server.js and added html router

--- Develop/server.js
+++ Develop/server.js
@@ -1,28 +1,13 @@
 const express = require('express');
 const path = require('path');
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
-
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
-
 // serves the static files from the public folder
 app.use(express.static('public'));
-
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-// GET Route for feedback page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
